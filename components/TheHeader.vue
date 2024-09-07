23cd32c8f93b983408cc980ff08708d7b7c16dc3
qleager <qleager@icloud.com>
2024-09-07 15:21:37 +0300

    feat(components): create header

---
+++ components/TheHeader.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+<header class="header">
+  <NuxtLink
+    to="/"
+    class="header__logo">
+    PET-NUXT-AUTH-PROFILE
+  </NuxtLink>
+  
+  <Avatar label="V" class="mr-2" size="large" style="background-color: #ece9fc; color: #2a1261" shape="circle" />
+</header>
+</template>
+
+<style>
+.header {
+  display: flex;
+  width: 100%;
+  height: 60px;
+  padding: 0 16px;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
