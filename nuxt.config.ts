6d98b4698e30d11a37dbc3bec062c5af67c35b04
qleager <qleager@icloud.com>
2024-09-07 16:14:26 +0300

    build: add eslint formatter rule

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@primevue/nuxt-module'],
-  eslint: {},
+  eslint: {
+    config: {
+        stylistic: true
+    }
+  },
   primevue: {
     options: {
         theme: {
