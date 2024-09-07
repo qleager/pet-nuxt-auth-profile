2adc29fcf35b26085ea5478a719f67a051dc00ec
qleager <qleager@icloud.com>
2024-09-07 15:18:18 +0300

    build: aff theme to primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primevue/themes/aura';
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@primevue/nuxt-module'],
-  eslint: {}
+  eslint: {},
+  primevue: {
+    options: {
+        theme: {
+            preset: Aura
+        }
+    }
+}
 })
